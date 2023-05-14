dd21581cb2b4e53eb94a656c47c5ac4b9c669a73
Toby Osborne <tosb789@aucklanduni.ac.nz>
2023-05-14 14:31:59 +1200

    python: added g-code-sender

---
+++ Web-Client/js/config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+const SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+const API_KEY = '47677191';
+const SESSION_ID = '2_MX40NzY3NzE5MX5-MTY4NDAyOTQ3NjY4N35jZXlBZlVCaTgwZDdvK2MvVjV5dDd3L0t-fn4';
+const TOKEN = 'T1==cGFydG5lcl9pZD00NzY3NzE5MSZzaWc9YTcwNDY5ZmM5MTcxZDRjODA0NDQ2ODkyMDkzOTQxM2M2ZGFhY2IxMDpzZXNzaW9uX2lkPTJfTVg0ME56WTNOekU1TVg1LU1UWTROREF5T1RRM05qWTROMzVqWlhsQlpsVkNhVGd3WkRkdksyTXZWalY1ZERkM0wwdC1mbjQmY3JlYXRlX3RpbWU9MTY4NDAyOTU0NyZub25jZT0wLjE0NjczNDQ4ODcxOTkxMzEzJnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE2ODQwMzMxNDUmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=';
