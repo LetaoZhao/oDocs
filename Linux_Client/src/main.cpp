eddb30b6f7c26d18f48989a6bc706a5e4f9a7d96
tazukiswift <tosb789@aucklanduni.ac.nz>
2023-08-02 17:19:19 +1200

    added  custom video publisher to opentok module

--- Linux_Client/src/main.cpp
+++ Linux_Client/src/main.cpp
@@ -5,18 +5,28 @@
 #include <iostream>
 #include <fstream>
 #include <thread>
+#include "otk_thread.h"
 
 #include "renderer.h"
 #include "gantry_interface.h"
 
 static std::atomic<bool> g_is_connected(false);
-
+static otc_publisher *g_publisher = nullptr;
+static std::atomic<bool> g_is_publishing(false);
 // Don't tell anyone but...
 GantryInterface gantry;
 
+// Callbacks for receiving messages
 static void on_session_connected(otc_session *session, void *user_data) {
     std::cout << __FUNCTION__ << " callback function" << std::endl;
     g_is_connected = true;
+    if ((session != nullptr) && (g_publisher != nullptr)) {
+        if (otc_session_publish(session, g_publisher) == OTC_SUCCESS) {
+            g_is_publishing = true;
+            return;
+        }
+        std::cout << "Could not publish successfully" << std::endl;
+    }
 }
 
 static void on_session_connection_created(otc_session *session,
@@ -69,8 +79,135 @@ static void on_session_error(otc_session *session,
     std::cout << "Session error. Error is: " << error_string << std::endl;
 }
 
+// Callbacks for Publishing a stream
 static void on_otc_log_message(const char *message) {
     std::cout << __FUNCTION__ << ":" << message << std::endl;
+}
+
+static void on_publisher_stream_created(otc_publisher *publisher,
+                                        void *user_data,
+                                        const otc_stream *stream) {
+    std::cout << __FUNCTION__ << " callback function" << std::endl;
+}
+
+static void on_publisher_render_frame(otc_publisher *publisher,
+                                      void *user_data,
+                                      const otc_video_frame *frame) {
+    auto *render_manager = static_cast<RendererManager*>(user_data);
+    if (render_manager == nullptr) {
+        return;
+    }
+    render_manager->addFrame(publisher, frame);
+}
+
+static void on_publisher_stream_destroyed(otc_publisher *publisher,
+                                          void *user_data,
+                                          const otc_stream *stream) {
+    std::cout << __FUNCTION__ << " callback function" << std::endl;
+}
+
+static void on_publisher_error(otc_publisher *publisher,
+                               void *user_data,
+                               const char* error_string,
+                               enum otc_publisher_error_code error_code) {
+    std::cout << __FUNCTION__ << " callback function" << std::endl;
+    std::cout << "Publisher error. Error code: " << error_string << std::endl;
+}
+//////////////////////////////////////////
+// Custom video interpreter for droidcam//
+//////////////////////////////////////////
+struct custom_video_capturer {
+    const otc_video_capturer *video_capturer;
+    struct otc_video_capturer_callbacks video_capturer_callbacks;
+    int width;
+    int height;
+    otk_thread_t capturer_thread;
+    std::atomic<bool> capturer_thread_exit;
+};
+
+static int generate_random_integer() {
+    srand(time(nullptr));
+    return rand();
+}
+
+static otk_thread_func_return_type capturer_thread_start_function(void *arg) {
+    struct custom_video_capturer *video_capturer = static_cast<struct custom_video_capturer *>(arg);
+    if (video_capturer == nullptr) {
+        otk_thread_func_return_value;
+    }
+
+    uint8_t *buffer = (uint8_t *)malloc(sizeof(uint8_t) * video_capturer->width * video_capturer->height * 4);
+
+    while(video_capturer->capturer_thread_exit.load() == false) {
+        memset(buffer, generate_random_integer() & 0xFF, video_capturer->width * video_capturer->height * 4);
+        otc_video_frame *otc_frame = otc_video_frame_new(OTC_VIDEO_FRAME_FORMAT_ARGB32, video_capturer->width, video_capturer->height, buffer);
+        otc_video_capturer_provide_frame(video_capturer->video_capturer, 0, otc_frame);
+        if (otc_frame != nullptr) {
+            otc_video_frame_delete(otc_frame);
+        }
+        usleep(1000 / 30 * 1000);
+    }
+
+    if (buffer != nullptr) {
+        free(buffer);
+    }
+
+    otk_thread_func_return_value;
+}
+
+static otc_bool video_capturer_init(const otc_video_capturer *capturer, void *user_data) {
+    struct custom_video_capturer *video_capturer = static_cast<struct custom_video_capturer *>(user_data);
+    if (video_capturer == nullptr) {
+        return OTC_FALSE;
+    }
+
+    video_capturer->video_capturer = capturer;
+
+    return OTC_TRUE;
+}
+
+static otc_bool video_capturer_destroy(const otc_video_capturer *capturer, void *user_data) {
+    struct custom_video_capturer *video_capturer = static_cast<struct custom_video_capturer *>(user_data);
+    if (video_capturer == nullptr) {
+        return OTC_FALSE;
+    }
+
+    video_capturer->capturer_thread_exit = true;
+    otk_thread_join(video_capturer->capturer_thread);
+
+    return OTC_TRUE;
+}
+
+static otc_bool video_capturer_start(const otc_video_capturer *capturer, void *user_data) {
+    struct custom_video_capturer *video_capturer = static_cast<struct custom_video_capturer *>(user_data);
+    if (video_capturer == nullptr) {
+        return OTC_FALSE;
+    }
+
+    video_capturer->capturer_thread_exit = false;
+    if (otk_thread_create(&(video_capturer->capturer_thread), &capturer_thread_start_function, (void *)video_capturer) != 0) {
+        return OTC_FALSE;
+    }
+
+    return OTC_TRUE;
+}
+
+static otc_bool get_video_capturer_capture_settings(const otc_video_capturer *capturer,
+                                                    void *user_data,
+                                                    struct otc_video_capturer_settings *settings) {
+    struct custom_video_capturer *video_capturer = static_cast<struct custom_video_capturer *>(user_data);
+    if (video_capturer == nullptr) {
+        return OTC_FALSE;
+    }
+
+    settings->format = OTC_VIDEO_FRAME_FORMAT_ARGB32;
+    settings->width = video_capturer->width;
+    settings->height = video_capturer->height;
+    settings->fps = 30;
+    settings->mirror_on_local_render = OTC_FALSE;
+    settings->expected_delay = 0;
+
+    return OTC_TRUE;
 }
 
 // Cursed
@@ -108,6 +245,25 @@ int main(int argc, char **argv) {
     session_callbacks.on_disconnected = on_session_disconnected;
     session_callbacks.on_error = on_session_error;
 
+    // Publisher
+    struct otc_publisher_callbacks publisher_callbacks = {0};
+    publisher_callbacks.user_data = &renderer_manager;
+    publisher_callbacks.on_stream_created = on_publisher_stream_created;
+    publisher_callbacks.on_render_frame = on_publisher_render_frame;
+    publisher_callbacks.on_stream_destroyed = on_publisher_stream_destroyed;
+    publisher_callbacks.on_error = on_publisher_error;
+
+    // Custom Video Capturer
+    struct custom_video_capturer *video_capturer = (struct custom_video_capturer *)malloc(sizeof(struct custom_video_capturer));
+    video_capturer->video_capturer_callbacks = {0};
+    video_capturer->video_capturer_callbacks.user_data = static_cast<void *>(video_capturer);
+    video_capturer->video_capturer_callbacks.init = video_capturer_init;
+    video_capturer->video_capturer_callbacks.destroy = video_capturer_destroy;
+    video_capturer->video_capturer_callbacks.start = video_capturer_start;
+    video_capturer->video_capturer_callbacks.get_capture_settings = get_video_capturer_capture_settings;
+    video_capturer->width = 1280;
+    video_capturer->height = 720;
+
     otc_session *session = nullptr;
     std::fstream config_file;
     config_file.open("../session.cfg", std::ios::in);
@@ -130,6 +286,18 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
+    // Register Publisher
+    g_publisher = otc_publisher_new("opentok-linux-sdk-samples",
+                                    &(video_capturer->video_capturer_callbacks), /* Use WebRTC's video capturer. */
+                                    &publisher_callbacks);
+    if (g_publisher == nullptr) {
+        std::cout << "Could not create OpenTok publisher successfully" << std::endl;
+        otc_session_delete(session);
+        return EXIT_FAILURE;
+    }
+
+    renderer_manager.createRenderer(g_publisher);
+
     otc_session_connect(session, TOKEN.c_str());
 
     std::thread system_loop(system_loop_wrapper);
@@ -137,6 +305,17 @@ int main(int argc, char **argv) {
     std::thread event_loop(event_loop_wrapper);
 
     event_loop.join();
+
+    renderer_manager.destroyRenderer(g_publisher);
+
+    if ((session != nullptr) && (g_publisher != nullptr) && g_is_publishing.load()) {
+        otc_session_unpublish(session, g_publisher);
+    }
+
+    if (g_publisher != nullptr) {
+        otc_publisher_delete(g_publisher);
+        g_publisher = nullptr;
+    }
 
     if ((session != nullptr) && g_is_connected.load()) {
         otc_session_disconnect(session);
