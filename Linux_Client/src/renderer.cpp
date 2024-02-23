c5a198fbaf48fd99472b3a025c01d8bf07670cbf
Letao Zhao <lzha850@aucklanduni.ac.nz>
2024-02-23 14:18:22 +1300

    testvs

--- Linux_Client/src/renderer.cpp
+++ Linux_Client/src/renderer.cpp
@@ -66,7 +66,7 @@ void RendererManager::handleCustomEvent(const SDL_Event &event) {
 }
 
 void RendererManager::createRenderer(void *key) {
-    std::cout << "CreateRenderer with key: " << key << std::endl;
+    std::cout << "CreateRenderer with: " << key << std::endl;
     pushEvent(kRendererCreate, key, NULL);
     std::cout << "CreateRenderer" << std::endl;
 }
